7b2f48ac331d26046e0abaf0e585719ef4e9017e
Fyrgeit <mans@peronilla.se>
2023-11-24 10:30:56 +0100

    Added styling and more properties to the class

--- scripts/economicsClasses.js
+++ scripts/economicsClasses.js
@@ -1,4 +1,4 @@
-export class EconomyObject {
+export class BalanceObject {
     // "first" part
     propertyAssets = { name: "Anläggningstillgångar", value: 100 };
     revenueAssets = { name: "Omsättningstillgångar", value: 100 };
@@ -8,4 +8,24 @@ export class EconomyObject {
     longtermDebt = { name: "Långsiktiga skulder", value: -200 };
     shorttermDebt = { name: "Kortfristiga skulder", value: -100 };
     debtSum = { name: "Summa; skulder och kapital", value: 0 };
-}
+};
+
+export class ResultObject {
+    salesPayments = { name: "Försäljningsinbetalningar", value: 0 };
+    customerFordrings = { name: "Kundfordringar", value: 0 };
+    salesSum = { name: "Totala försäljningsintäkter", value: 0 };
+    
+    costSoldProducts = { name: "Kostnad sålda varor", value: 0 };
+    grossSum = { name: "Bruttoresultat", value: 0 };
+
+    writeOffs = { name: "Avskrivningar", value: 0 };
+    interestFinancialIncome = { name: "Ränte- och finansiella intäkter", value: 0 };
+    interestFinancialCosts = { name: "Ränte- och finansiella kostnader", value: 0 };
+    taxCosts = { name: "Skattekostnader", value: 0 };
+    yearSum = { name: "Årets resultat", value: 0 };
+
+    investments = { name: "Investeringar", value: 0 };
+    allotment = { name: "Utdelning till ägare", value: 0 };
+    newIssue = { name: "Nyemission", value: 0 };
+    newLoans = { name: "Nya banklån", value: 0 };
+};
